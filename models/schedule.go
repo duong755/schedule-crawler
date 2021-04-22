d4734609086c754fdd641f4cd28559bb49b72c7a
duong755 <ngo.quang.duong.1100@gmail.com>
2021-04-22 12:53:05 +0700

    Add bson, json tag for Classes; Use DateTime type for birthday

--- models/schedule.go
+++ models/schedule.go
@@ -5,15 +5,15 @@ import (
 )
 
 type Student struct {
-	Id       string `bson:"id" json:"id"`
-	Name     string `bson:"name" json:"name"`
-	Birthday string `bson:"birthday" json:"birthday"`
-	Course   string `bson:"course" json:"course"`
-	Note     string `bson:"note" json:"note"`
+	Id       string             `bson:"id" json:"id"`
+	Name     string             `bson:"name" json:"name"`
+	Birthday primitive.DateTime `bson:"birthday" json:"birthday"`
+	Course   string             `bson:"course" json:"course"`
+	Note     string             `bson:"note" json:"note"`
 }
 
 type Schedule struct {
-	Id      primitive.ObjectID `bson:"id" json:"id"`
-	Student Student            `bson:"student" json:"student"`
-	Classes []primitive.ObjectID
+	Id      primitive.ObjectID   `bson:"id" json:"id"`
+	Student Student              `bson:"student" json:"student"`
+	Classes []primitive.ObjectID `bson:"classes" json:"classes"`
 }
